63f07fc0af5bc1bed03c414a574764f643e6ac57
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-09-05 00:00:16 +0100

    Added basic styling to page

---
+++ user.js
@@ -0,0 +1,45 @@
+const user = {
+  id: 10,
+  userName: "tommy",
+  createdAt: "datestring",
+  email: "tommy@gmail.com",
+  diet: {
+    isVegan: false,
+    isVeggie: false,
+    redMeatConsumption: 10,
+    localProduce: 10,
+  },
+  recyling: {
+    electic: 0,
+    plastic: 50,
+    paper: 100,
+  },
+  houseEnergy: {
+    electrictiy: 10,
+    gas: 10,
+    oil: 10,
+    coal: 10,
+    wood: 10,
+    solar: 10,
+    isInsulation: true,
+  },
+  transport: {
+    flightsPerYear: 10,
+    flightDistanceEst: 10,
+    carMilagePerYear: 10,
+    vehicle: "car",
+    isElectric: false,
+    travelPercentKM: {
+      cyling: 10,
+      bus: 10,
+      rail: 10,
+      car: 60,
+      walking: 10,
+    },
+  },
+  activity: {
+    treeplanting: true,
+    volunteering: true,
+    donation: 100,
+  },
+};

23847534bc2c62dc441bf8653d7198ec435f9aeb
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-09-05 14:33:04 +0100

    Added retreive user data on auth

--- src/assets/js/auth.js
+++ src/assets/js/auth.js
@@ -1,10 +1,18 @@
 import { onAuthStateChanged } from "firebase/auth";
-import { firebaseAuth } from "../../config/firebase";
+import { firebaseAuth, firebaseDB } from "../../config/firebase";
+import { doc, getDoc } from "@firebase/firestore";
 
 export const checkAuthState = async () => {
-  onAuthStateChanged(firebaseAuth, (user) => {
+  onAuthStateChanged(firebaseAuth, async (user) => {
     if (user) {
       console.log(user);
+      const userData = await getUserData(user);
+
+      console.log(userData);
+
+      const profileIcon = document.getElementById("profile");
+      profileIcon.innerHTML = `<i class="fa-solid fa-user"></i
+      > ${userData.name}`;
     } else {
       console.log("user is not logged in");
     }
@@ -12,3 +20,17 @@ export const checkAuthState = async () => {
 };
 
 checkAuthState();
+
+async function getUserData(user) {
+  try {
+    const userRef = doc(firebaseDB, "users", user.uid);
+    const userSnapshot = await getDoc(userRef);
+    if (userSnapshot.exists()) {
+      return userSnapshot.data();
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.log("Error retrieving user data:", error);
+  }
+}

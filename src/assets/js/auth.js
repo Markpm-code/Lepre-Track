8de24a95276ba7bf88965c408705fdd716e3aee9
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-09-05 15:03:17 +0100

    Promisified on auth state function for async await

--- src/assets/js/auth.js
+++ src/assets/js/auth.js
@@ -2,23 +2,31 @@ import { onAuthStateChanged } from "firebase/auth";
 import { firebaseAuth, firebaseDB } from "../../config/firebase";
 import { doc, getDoc } from "@firebase/firestore";
 
-export const checkAuthState = async () => {
-  onAuthStateChanged(firebaseAuth, async (user) => {
-    if (user) {
-      const userData = await getUserData(user);
+export const checkAuthState = () => {
+  return new Promise((resolve, reject) => {
+    onAuthStateChanged(firebaseAuth, async (user) => {
+      if (user) {
+        try {
+          const userData = await getUserData(user);
 
-      const profileIcon = document.getElementById("profile");
-      profileIcon.innerHTML = `<i class="fa-solid fa-user"></i
-      > ${userData.name}`;
-    } else {
-      console.log("user is not logged in");
-    }
+          const profileIcon = document.getElementById("profile");
+          profileIcon.innerHTML = `<i class="fa-solid fa-user"></i> ${userData.name}`;
+
+          resolve(user);
+        } catch (error) {
+          console.error("Error getting user data:", error);
+          reject(error);
+        }
+      } else {
+        console.log("User is not logged in");
+        resolve(null);
+      }
+    });
   });
 };
-
 checkAuthState();
 
-async function getUserData(user) {
+export async function getUserData(user) {
   try {
     const userRef = doc(firebaseDB, "users", user.uid);
     const userSnapshot = await getDoc(userRef);
---
+++ src/assets/js/profile.js
@@ -0,0 +1,13 @@
+import { checkAuthState, getUserData } from "./auth";
+
+const welcomeText = document.getElementById("welcomeText");
+
+async function renderWelcomeText() {
+  const user = await checkAuthState();
+  console.log(user);
+  const userData = await getUserData(user);
+
+  welcomeText.innerText = `Welcome back ${userData.name}`;
+}
+
+renderWelcomeText();
